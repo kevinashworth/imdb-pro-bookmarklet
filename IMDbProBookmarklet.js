61aae1ac551f98e3856a1363fa995f71d19e56a3
Kevin Ashworth <kevinashworth@yahoo.com>
2020-03-12 14:27:45 -0700

    Another June 2013 edit
    And delete that old file

--- IMDbProBookmarklet.js
+++ IMDbProBookmarklet.js
@@ -1,16 +1,23 @@
 The bookmarklet:
-javascript:h=location.href;i=h.indexOf(%22imdb.com%22);p=h.indexOf(%22http://pro.imdb.com%22);t=h.indexOf(%22title%22);c=h.indexOf(%22combined%22);if((t>1)&&(c>1)){u=h.substring(0,c)};if(i==-1){window.location=%22http://pro.imdb.com/name/nm2825198/%22};if(p==-1){window.location=u.replace(/http:\/\/[a-z]+/,%22http://pro%22)};
-
-
+javascript:h=location.href;i=h.indexOf(%27imdb.com%27);p=h.indexOf(%27http://pro.imdb.com%27);t=h.indexOf(%27title%27);c=h.indexOf(%27combined%27);if(i==-1){window.location=%27http://pro.imdb.com/name/nm2825198/%27}else%20if(p==0){window.location=h.replace(%27http://pro%27,%27http://www%27)}else%20if(p==-1){if((t>1)&&(c>1)){h=h.substring(0,c)};window.location=h.replace(/http:\/\/[a-z]+/,%27http://pro%27);};
 
 
 The bookmarklet with line breaks and unencoded:
-javascript:
+
 h=location.href;
-i=h.indexOf("imdb.com");
-p=h.indexOf("http://pro.imdb.com");
-t=h.indexOf("title");
-c=h.indexOf("combined");
-if((t>1)&&(c>1)){u=h.substring(0,c)};
-if(i==-1){window.location="http://pro.imdb.com/name/nm2825198/"};
-if(p==-1){window.location=u.replace(/http:\/\/[a-z]+/,"http://pro")};
+i=h.indexOf('imdb.com');
+p=h.indexOf('http://pro.imdb.com');
+t=h.indexOf('title');
+c=h.indexOf('combined');
+if(i==-1){
+  window.location='http://pro.imdb.com/name/nm2825198/'
+}
+else if(p==0){
+  window.location=h.replace('http://pro','http://www')
+}
+else if(p==-1){
+  if((t>1)&&(c>1)){
+    h=h.substring(0,c)
+  };
+  window.location=h.replace(/http:\/\/[a-z]+/,'http://pro');
+};
