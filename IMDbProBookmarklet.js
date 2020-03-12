12489e1a9e08329b8723312501b61d4a68f2b569
Kevin Ashworth <kevinashworth@yahoo.com>
2020-03-12 14:23:39 -0700

    3rd Edit Oct 2010

--- IMDbProBookmarklet.js
+++ IMDbProBookmarklet.js
@@ -1,1 +1,1 @@
-javascript:h=location.href;i=h.indexOf(%22imdb.com%22);p=h.indexOf(%22http://pro.imdb.com%22);if(i==-1){window.location=%22http://pro.imdb.com/name/nm2825198/%22}else%20if(p==-1){window.location=h.replace(/http:\/\/[a-z]+/,%22http://pro%22)};
+javascript:h=location.href;i=h.indexOf(%22imdb.com%22);p=h.indexOf(%22http://pro.imdb.com%22);t=h.indexOf(%22title%22);c=h.indexOf(%22combined%22);if((t>1)&&(c>1)){u=h.substring(0,c)};if(i==-1){window.location=%22http://pro.imdb.com/name/nm2825198/%22}else%20if(p==-1){window.location=u.replace(/http:\/\/[a-z]+/,%22http://pro%22)};
